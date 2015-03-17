cd7be484755cd63ccefc72182748abe42a4b743e
kidynamit <paul.mwanjohi@gmail.com>
2015-03-17 16:15:01 +0200

    owlapi included

---
+++ Tutorial.java
@@ -0,0 +1,9 @@
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+
+public class Tutorial {
+    public static final IRI ncs_iri = IRI
+        .create("http://www.meteck.org/files/ontologies/ncs.owl");
+
+}
+
